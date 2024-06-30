47b75442962f0f5f8b59480fb0b5c806426ff510
ArnavTapadia <at565@cornell.edu>
2024-06-30 18:26:38 +0800

    starting feature engineering

---
+++ time_series_predictor/Data/emotionFeatureExtractor.py
@@ -0,0 +1,9 @@
+import numpy as np
+import torch
+import json
+
+
+class emotionFeatureExtractor:
+    
+    def read_emotion_logs(logsFilePath = 'time_series_predictor/Data'):
+        raise NotImplementedError
