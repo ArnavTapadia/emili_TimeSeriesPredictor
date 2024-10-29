8d6b21fc7fe69a28134e410c45db548ec2f70d82
ArnavTapadia <at565@cornell.edu>
2024-10-29 19:08:10 -0400

    option to do hidden layers and activation through regular lstm. Not very useful

--- time_series_predictor/Models/lstm_models.py
+++ time_series_predictor/Models/lstm_models.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 '''
@@ -82,7 +83,7 @@ LSTMUnivariateMultiStep: predicts forecast_length timesteps into the future in o
 '''
 
 class LSTMUnivariateMultiStep(nn.Module):
-    def __init__(self, timesteps, features, lstm_units=64, forecast_length=1):
+    def __init__(self, timesteps, features, lstm_units=64, hidden_fc_layers = 0, hidden_units = 128, forecast_length=1, nn_activation = 'linear'):
         """
         LSTM Model for univariate time series with multi-step one-shot prediction.
 
@@ -91,12 +92,20 @@ class LSTMUnivariateMultiStep(nn.Module):
         - features (int): Number of features at each timestep.
         - lstm_units (int): Number of units in the LSTM layer.
         - forecast_length (int): Number of steps to predict into the future (in one shot).
+        - hidden_fc_layers (int): Number of Fully Connected hidden NN layers after the LSTM layer
+        - hidden_units (int): Number of nodes in the hidden layers
+        - nn_activation (string): Activation to use on the last fully connected output layer
         """
         super(LSTMUnivariateMultiStep, self).__init__()
 
         self.timesteps = timesteps
         self.features = features
         self.lstm_units = lstm_units
+        self.hidden_units = hidden_units
+        assert hidden_fc_layers == 0 or hidden_fc_layers < 3 #only supports 0 or 1 right now
+        self.hidden_fc_layers = hidden_fc_layers
+        assert nn_activation == 'relu' or nn_activation == 'linear' #only supports linear or relu now
+        self.nn_activation = nn_activation
         self.forecast_length = forecast_length  # Number of timesteps to predict
 
         # LSTM layer: input_size is features, hidden_size is lstm_units
@@ -104,7 +113,12 @@ class LSTMUnivariateMultiStep(nn.Module):
 
         # Fully connected layer: Maps LSTM output to future timestep predictions
         # Instead of just mapping to features, we now map to forecast_length * features
-        self.fc = nn.Linear(lstm_units, forecast_length * features)
+        self.fc1 = nn.Linear(lstm_units, hidden_units)
+        self.fc2 = nn.Linear(hidden_units,hidden_units)
+        if hidden_fc_layers > 0:
+            self.output = nn.Linear(hidden_units, forecast_length * features)
+        else:
+            self.output = nn.Linear(lstm_units, forecast_length * features)
 
     def forward(self, x):
         """
@@ -123,8 +137,90 @@ class LSTMUnivariateMultiStep(nn.Module):
         # Use the output from the last timestep of the LSTM
         last_timestep_output = lstm_out[:, -1, :]  # shape: (batch_size, lstm_units)
 
+        if self.hidden_fc_layers == 1:
+            hidden_out = F.relu(self.fc1(last_timestep_output))
+        elif self.hidden_fc_layers == 2:
+            hidden_out1 = F.relu(self.fc1(last_timestep_output))
+            hidden_out = F.relu(self.fc2(hidden_out1))
+        else:
+            hidden_out = last_timestep_output
+        
+
         # Pass through the fully connected layer to predict the full forecast
-        fc_output = self.fc(last_timestep_output)  # shape: (batch_size, forecast_length * features)
+        if self.nn_activation == 'relu':
+            fc_output = F.relu(self.output(hidden_out))  # shape: (batch_size, forecast_length * features)
+        else:
+            fc_output = self.output(hidden_out)
+
+        # Reshape the output to be (batch_size, forecast_length, features)
+        output = fc_output.view(-1, self.forecast_length, self.features)
+
+        return output  # shape: (batch_size, forecast_length, features)
+
+
+'''
+AugmentedLSTM: similar to the univariate multistep LSTM, 
+this architecture augments the LSTM output with the last few points (nAugmentLength)
+'''
+
+class AugmentedLSTM(nn.Module):
+    def __init__(self, timesteps, features, nAugmentLength=100, lstm_units=64, forecast_length=1, nn_activation = 'linear'):
+        """
+        LSTM Model for univariate time series with multi-step one-shot prediction.
+
+        Parameters:
+        - timesteps (int): Number of timesteps in the input sequence.
+        - features (int): Number of features at each timestep.
+        - lstm_units (int): Number of units in the LSTM layer.
+        - forecast_length (int): Number of steps to predict into the future (in one shot).
+        - nAugmentLength (int): Number of timesteps to take raw values from
+        """
+        super(AugmentedLSTM, self).__init__()
+
+        self.timesteps = timesteps
+        self.features = features
+        self.lstm_units = lstm_units
+        self.nAugmentLength = nAugmentLength
+        assert nn_activation == 'relu' or nn_activation == 'linear' #only supports linear or relu now
+        self.nn_activation = nn_activation
+        self.forecast_length = forecast_length  # Number of timesteps to predict
+
+        # LSTM layer: input_size is features, hidden_size is lstm_units
+        self.lstm = nn.LSTM(input_size=features, hidden_size=lstm_units, batch_first=True)
+
+        # Fully connected layer: Maps LSTM output to future timestep predictions
+        # Instead of just mapping to features, we now map to forecast_length * features
+
+        self.output = nn.Linear(lstm_units + self.nAugmentLength, forecast_length * features)
+
+    def forward(self, x):
+        """
+        Forward pass of the model.
+        
+        Parameters:
+        - x (torch.Tensor): Input tensor of shape (batch_size, timesteps, features).
+        
+        Returns:
+        - torch.Tensor: Predicted output for the next `forecast_length` timesteps,
+                        shape (batch_size, forecast_length, features).
+        """
+        # Pass input through LSTM layer
+        lstm_out, _ = self.lstm(x)  # lstm_out shape: (batch_size, timesteps, lstm_units)
+
+        augmentValues = torch.squeeze(x[:,-self.nAugmentLength:,:],dim=2) #last timestep values we take to augment LSTM ouput
+        #shape: (batch_size,self.nAugmentLength)
+
+        # Use the output from the last timestep of the LSTM
+        last_timestep_output = lstm_out[:, -1, :]  # shape: (batch_size, lstm_units)
+
+        assert augmentValues.shape[0] == last_timestep_output.shape[0]
+        augmented_output = torch.cat((last_timestep_output,augmentValues), dim=1)
+
+        # Pass through the fully connected layer to predict the full forecast
+        if self.nn_activation == 'relu':
+            fc_output = F.relu(self.output(augmented_output))  # shape: (batch_size, forecast_length * features)
+        else:
+            fc_output = self.output(augmented_output)
 
         # Reshape the output to be (batch_size, forecast_length, features)
         output = fc_output.view(-1, self.forecast_length, self.features)
